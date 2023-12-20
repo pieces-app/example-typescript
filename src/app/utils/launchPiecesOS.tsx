3213eea15dc10b186cf7b406c8776c1182072999
agrim0312 <agrim.12saxena@gmail.com>
2023-12-20 15:55:28 +0530

    updates-button-automatically

--- src/app/utils/launchPiecesOS.tsx
+++ src/app/utils/launchPiecesOS.tsx
@@ -1,6 +1,6 @@
 import { connect } from './Connect';; // Import the Promise type
 
-const launchPiecesOS = () => {
+const launchPiecesOS = async () => {
   const url: string = 'pieces://launch';
   try {
     const isPiecesOSConnected = navigator.userAgent.includes('PiecesOS');
@@ -8,12 +8,27 @@ const launchPiecesOS = () => {
       console.log("Pieces OS is already connected.");
       // Perform any additional actions for connected state
     } else {
-      window.open(url, '_blank');
+      await window.open(url, '_blank');
       console.log("Pieces OS is installed.");
       window.location.reload();// Refresh the page
     }
   } catch {
     console.error("Pieces OS is not installed.");
+  }
+  finally {
+    connect().then((data: JSON) => {
+      // define the indicator now that it exists.
+      const _indicator = document.getElementById("indicator");
+
+      // conditional for the response back on application.
+      //
+      // (1) first @jordan-pieces came in here and added this turing statement here inside a new
+      // if statement. this is an upgrade in comparison to the previous if statement that would not check to
+      // see if the _indicator itself is added to the DOM yet.
+      if (_indicator != null) {
+        _indicator != undefined ? _indicator.style.backgroundColor = "green" : _indicator.style.backgroundColor = "red";
+      }
+    })
   } // Add a missing closing parenthesis and semicolon here
 }
 export { launchPiecesOS };
