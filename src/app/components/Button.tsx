f10c060d80422b1473eeb40b032700f4aaeb91ae
pythonistical <142655673+pythonistical@users.noreply.github.com>
2024-01-09 08:51:33 +0530

    Update Button.tsx
    Creating the new main button style

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,22 +1,37 @@
-import * as React from 'react';
-import { createAsset } from './Asset';
-
-export function CreateButton({applicationData, data, name}): React.JSX.Element {
-  return (
-      <button  style={{
-          marginTop: '10px',
-        marginBottom: '10px',
-        maxWidth: 'fit-content',
-        height: 'fit-content',
-        // marginLeft: '10px',
-        backgroundColor: "black",
-        border: '1px solid white',
-        borderRadius: '5px',
-        padding: '8px 24px',
-        color: 'white',
-        flexWrap: 'nowrap',
-        cursor: 'pointer',
-        fontSize: '12px'
-      }} onClick={() => createAsset(applicationData, data, name)}>Create Snippet</button>
-  )
+<html>
+    <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+        <style>
+            body{font-family: "poppins",serif}
+            .button {
+  background-color: #04AA6D; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  border-radius: 10%
 }
+.button_design {
+  background-color: white;
+  color: black;
+  border: 2px solid #555555;
+}
+
+.button_design:hover {
+  background-color: #555555;
+  color: white;
+}
+</style>
+</head>
+<body>
+    <span class="button button_design">Pieces</span>
+</body>
+</style>
+</head>
+</html>
