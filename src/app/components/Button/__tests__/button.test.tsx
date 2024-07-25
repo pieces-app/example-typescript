3575381d61b40e198d869f45d1acd1a8903286f6
Srini Pandiyan <srinipandiyan7@gmail.com>
2024-07-25 16:59:13 -0500

    Addressed PR comments

--- src/app/components/Button/__tests__/button.test.tsx
+++ src/app/components/Button/__tests__/button.test.tsx
@@ -1,33 +1,15 @@
 import * as React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { CreateButton } from '../Button';
-import '@testing-library/jest-dom'
-
-// Mocking of the createAsset function
-jest.mock('../Asset/Asset', () => ({
-  createAsset: jest.fn()
-}));
+import '@testing-library/jest-dom';
 
 // Resets mock function call counts following each test
 describe('CreateButton', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
 
   it('should render a button labeled "Create Snippet"', () => {
     render(<CreateButton applicationData={null} data={null} name={null} />);
     const buttonElement = screen.getByRole('button', { name: /Create Snippet/i });
     expect(buttonElement).toBeInTheDocument();
-  });
-
-  it('should call createAsset when button is clicked with all parameters provided', () => {
-    const applicationData = { propApp: 'app' };
-    const data = { propData: 'data' };
-    const name = 'propName';
-    render(<CreateButton applicationData={applicationData} data={data} name={name} />);
-    const buttonElement = screen.getByRole('button', { name: /Create Snippet/i });
-    fireEvent.click(buttonElement);
-    expect(require('../Asset/Asset').createAsset).toHaveBeenCalledWith(applicationData, data, name);
   });
 
   it('should alert the user when any parameter is missing', () => {
@@ -36,5 +18,6 @@ describe('CreateButton', () => {
     const buttonElement = screen.getByRole('button', { name: /Create Snippet/i });
     fireEvent.click(buttonElement);
     expect(alertMock).toHaveBeenCalledWith("One or more parameters are empty or null.");
+    alertMock.mockRestore();
   });
 });
