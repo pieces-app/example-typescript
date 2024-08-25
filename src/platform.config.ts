a71c30ac68cec4bc1afa0d8766eb39cd6417da9d
Vishal Pawar <vshalofficla@gmail.com>
2024-08-25 22:01:19 +0530

    feat: added platform settings

--- src/platform.config.ts
+++ src/platform.config.ts
@@ -4,11 +4,27 @@ import { PiecesClient } from "pieces-copilot-sdk";
 let BASE_URL: string;
 let WS_URL: string;
 
-// Define your port here 
+// Define your port based on the operating system
+let PORT: number;
+
+// Define your port here
 // LINUX PORT=5323
 // For Windows and MacOs PORT=1000
 
-const PORT = 5323
+const platform = navigator.platform.toLowerCase();
+console.log("OS TYPE IS: ",platform)
+
+const userAgent = navigator.userAgent.toLowerCase();
+
+if (platform.includes('win')) {
+  PORT = 1000;
+} else if (platform.includes('mac')) {
+  PORT = 1000;
+} else if (platform.includes('linux')) {
+  PORT = 5323;
+} else {
+  console.log("PORT not set for : ",platform)
+}
 
 BASE_URL = 'http://localhost:'+PORT;
 WS_URL = 'ws://localhost:'+PORT;
